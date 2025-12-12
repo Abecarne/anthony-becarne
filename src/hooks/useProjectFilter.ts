0fb30615ca67cd866be2e858cf7f851629dd3bd0
Anthony Becarne <anthony.becarne@gmail.com>
2025-12-13 00:27:48 +0100

    fix: import types

--- src/hooks/useProjectFilter.ts
+++ src/hooks/useProjectFilter.ts
@@ -1,5 +1,5 @@
 import { useCallback, useMemo, useState } from "react";
-import { Category, WorkProject } from "./usePortfolioData";
+import type { Category, WorkProject } from "@/types";
 
 interface UseProjectFilterProps {
   projects: WorkProject[];
